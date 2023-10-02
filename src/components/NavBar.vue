7c45eee71dbacc28b1b9d943dbe5464470b8f668
Mario Aguilar <mario661962@yahoo.com>
2023-10-02 02:20:42 -0500

    added env file 2

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@ import { useAuth } from '@/components/composables/useAuth'
 
 const {isAuthenticated, logout, user} = useAuth()
 
-const brand =ref('⛵ Lake Company Directory')
+const brand =ref( import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
